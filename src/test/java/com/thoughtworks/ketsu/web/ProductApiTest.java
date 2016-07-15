a8d8bc865bf27170c1f70fb4a01f187aab5ca774
HFCherish <hf_cherish@163.com>
2016-07-15 17:46:27 +0800

    return empty when get all products given empty database

--- src/test/java/com/thoughtworks/ketsu/web/ProductApiTest.java
+++ src/test/java/com/thoughtworks/ketsu/web/ProductApiTest.java
@@ -16,7 +16,6 @@ import java.util.Map;
 
 import static com.thoughtworks.ketsu.support.TestHelper.*;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.number.IsCloseTo.closeTo;
@@ -54,7 +53,11 @@ public class ProductApiTest extends ApiSupport {
 
         assertThat(response.getStatus(), is(200));
         Map prodInfo = response.readEntity(Map.class);
-        assertThat(prodInfo.get("uri").toString(), is(getOneUrl));
+        verifyResponseProdInfo(product, getOneUrl, prodInfo);
+    }
+
+    private void verifyResponseProdInfo(Product product, String prodUrl, Map prodInfo) {
+        assertThat(prodInfo.get("uri").toString(), is(prodUrl));
         assertThat(prodInfo.get("name").toString(), is(product.getName()));
         assertThat((double)prodInfo.get("price"), is(closeTo(product.getPrice(), 0.01)));
         assertThat(prodInfo.get("description").toString(), is(product.getDescription()));
@@ -81,9 +84,16 @@ public class ProductApiTest extends ApiSupport {
         List products = response.readEntity(List.class);
         assertThat(products.size(), is(1));
         Map prodInfo = (Map)products.get(0);
-        assertThat(prodInfo.get("uri"), is(productsBaseUrl + "/" + product.getId()));
-        assertThat(prodInfo.get("name").toString(), is(product.getName()));
-        assertThat((double)prodInfo.get("price"), is(closeTo(product.getPrice(), 0.01)));
-        assertThat(prodInfo.get("description").toString(), is(product.getDescription()));
+        verifyResponseProdInfo(product, productsBaseUrl + "/" + product.getId(), prodInfo);
+    }
+
+    @Test
+    public void should_return_nothing_when_get_all_given_empty_db() {
+        Response response = target(productsBaseUrl).request().get();
+
+        assertThat(response.getStatus(), is(200));
+        List products = response.readEntity(List.class);
+        assertThat(products.size(), is(0));
+
     }
 }
